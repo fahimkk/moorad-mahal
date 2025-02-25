9896cb5ed5db6fda0ec0789d29b0d1a8539a33e0
fahimkk <fahimasharafkk@gmail.com>
2025-02-25 16:25:12 +0530

    fix(app): Fix any type build issue

--- src/store/useDonationStore.ts
+++ src/store/useDonationStore.ts
@@ -13,7 +13,6 @@ type Donation = {
 type DonationStore = {
   donations: Donation[];
   isDonationsLoading: boolean;
-  error: string | null;
   fetchDonations: () => Promise<void>;
 };
 
@@ -21,18 +20,17 @@ type DonationStore = {
 export const useDonationStore = create<DonationStore>((set) => ({
   donations: [],
   isDonationsLoading: false,
-  error: null,
 
   // Fetch Donations from API
   fetchDonations: async () => {
-    set({ isDonationsLoading: true, error: null });
+    set({ isDonationsLoading: true });
     try {
       const res = await fetch("/api/donations"); // API route
       if (!res.ok) throw new Error("Failed to fetch donations");
       const data: Donation[] = await res.json();
       set({ donations: data, isDonationsLoading: false });
-    } catch (error: any) {
-      set({ error: error.message, isDonationsLoading: false });
+    } catch (error) {
+        console.error("Failed to fetch donations", error);
     }
   },
 }));
